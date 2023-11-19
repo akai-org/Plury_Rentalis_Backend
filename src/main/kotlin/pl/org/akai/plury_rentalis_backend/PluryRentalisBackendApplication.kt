8d94e397999a555c1d9e63f83513118928f3d432
McSymilian <max.ryder8@gmail.com>
2023-11-19 15:25:27 +0100

    setup

---
+++ src/main/kotlin/pl/org/akai/plury_rentalis_backend/PluryRentalisBackendApplication.kt
@@ -0,0 +1,11 @@
+package pl.org.akai.plury_rentalis_backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PluryRentalisBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<PluryRentalisBackendApplication>(*args)
+}
---
+++ src/test/kotlin/pl/org/akai/plury_rentalis_backend/PluryRentalisBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package pl.org.akai.plury_rentalis_backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PluryRentalisBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
